0d1badb8804e817e40d3d52195b29d2eca648352
201503841 <susethgg@gmail.com>
2016-12-07 00:53:08 -0600

    Metodo 2 "Cantidad consonantes"

--- HojadeTrabajo/HojadeTrabajo/Form1.Designer.cs
+++ HojadeTrabajo/HojadeTrabajo/Form1.Designer.cs
@@ -31,6 +31,7 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // textBox1
@@ -53,16 +54,27 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(185, 265);
+            this.label1.Location = new System.Drawing.Point(230, 267);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(0, 32);
             this.label1.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(449, 340);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(262, 92);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "CANTIDAD CONSONANTES";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 31F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1203, 903);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.textBox1);
@@ -78,6 +90,7 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- HojadeTrabajo/HojadeTrabajo/Form1.cs
+++ HojadeTrabajo/HojadeTrabajo/Form1.cs
@@ -14,6 +14,8 @@ namespace HojadeTrabajo
     {
 
         string palabra, temporal;
+        int conta1, conta2, conta3,consonantes1;
+        
         public Form1()
         {
            
@@ -28,6 +30,12 @@ namespace HojadeTrabajo
             palabraalreves();
 
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            consonantes();
+        }
+
         private void palabraalreves()
         {
             palabra = textBox1.Text;
@@ -37,6 +45,25 @@ namespace HojadeTrabajo
             }
             label1.Text = temporal;
 
+        }
+        private void consonantes()
+        {
+            palabra = textBox1.Text;
+            for (int i= 0; i < palabra.Length; i++)
+            {
+                conta1++;
+            }
+            for (int i = 0; i < palabra.Length; i++)
+            {
+                if ((palabra[i] == 'a')|| (palabra[i]=='e')|| (palabra[i]=='i')||(palabra[i]=='o')||(palabra[i]=='u'))
+                    {
+                    conta2++;
+                }
+            }
+            //resta de las variables del contador
+            consonantes1= conta1 - conta2;
+            label1.Text = consonantes1.ToString();
+            
         }
 
     }
